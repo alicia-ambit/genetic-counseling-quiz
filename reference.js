30292a7e3ab0da3bf73862d0e887de69c5434c9f
alicia-at-ambit <alicia.chen@ambitinc.com>
2022-03-20 19:27:09 -0400

    test commit

---
+++ reference.js
@@ -0,0 +1,36 @@
+// test change
+// const query1 = '''query {
+//     me {
+//       name
+//     }
+    
+//     boards(ids:[13542, 68097]) {
+//     boards(ids: 2245472432) {
+//       name
+  
+//       columns {
+//         title
+//         id
+//         type
+//       }
+  
+//       groups {
+//       	title
+//         id
+//       }
+  
+//       items {
+//         name
+//         group {
+//           id
+//         }
+        
+//         column_values {
+//           id
+//           value
+//           text
+//         }
+//       }
+//     }
+//   }
+// '''
